1c174bc4e391a33ca1844a933b6c667cf3dbbae6
Ömer Hüseyin GÜL <omerhuseyingul@outlook.com>
2022-02-16 19:27:09 +0300

    Added Contact Form

---
+++ ClipBoardManager/Forms/Contact.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ClipBoardManager.Forms
+{
+    partial class Contact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Contact));
+            this.label1 = new System.Windows.Forms.Label();
+            this.DiscordBannerController = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gunaSeparator1 = new Guna.UI.WinForms.GunaSeparator();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.gunaSeparator2 = new Guna.UI.WinForms.GunaSeparator();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pcrMail = new System.Windows.Forms.PictureBox();
+            this.pcrTwitter = new System.Windows.Forms.PictureBox();
+            this.pcrInstagram = new System.Windows.Forms.PictureBox();
+            this.pcrGithub = new System.Windows.Forms.PictureBox();
+            this.pcrDiscord = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrMail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrTwitter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrInstagram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrGithub)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrDiscord)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(286, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contact via Discord";
+            // 
+            // DiscordBannerController
+            // 
+            this.DiscordBannerController.Enabled = true;
+            this.DiscordBannerController.Interval = 1;
+            this.DiscordBannerController.Tick += new System.EventHandler(this.DiscordBannerController_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(212, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "\"The developer is active on Discord in General.\"";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(254, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "About the Developer";
+            // 
+            // gunaSeparator1
+            // 
+            this.gunaSeparator1.LineColor = System.Drawing.Color.Silver;
+            this.gunaSeparator1.Location = new System.Drawing.Point(192, 36);
+            this.gunaSeparator1.Name = "gunaSeparator1";
+            this.gunaSeparator1.Size = new System.Drawing.Size(288, 10);
+            this.gunaSeparator1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(231, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Name and Surname: Ömer Hüseyin GÜL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(317, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Age: 18";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(225, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(233, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Study Focus: Backend Services, Public API";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(166, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(350, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Hobbies: Mathematics, Science, Programming, Entrepreneurship";
+            // 
+            // gunaSeparator2
+            // 
+            this.gunaSeparator2.LineColor = System.Drawing.Color.Silver;
+            this.gunaSeparator2.Location = new System.Drawing.Point(192, 180);
+            this.gunaSeparator2.Name = "gunaSeparator2";
+            this.gunaSeparator2.Size = new System.Drawing.Size(288, 10);
+            this.gunaSeparator2.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(254, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(182, 19);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Contact the Developer";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(163, 351);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(364, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "\"The developer is less active on these platforms than on Discord.\"";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(270, 336);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(152, 15);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Contact via Other Platforms";
+            // 
+            // pcrMail
+            // 
+            this.pcrMail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcrMail.Image = ((System.Drawing.Image)(resources.GetObject("pcrMail.Image")));
+            this.pcrMail.Location = new System.Drawing.Point(376, 380);
+            this.pcrMail.Name = "pcrMail";
+            this.pcrMail.Size = new System.Drawing.Size(25, 25);
+            this.pcrMail.TabIndex = 16;
+            this.pcrMail.TabStop = false;
+            this.pcrMail.Click += new System.EventHandler(this.pcrMail_Click);
+            // 
+            // pcrTwitter
+            // 
+            this.pcrTwitter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcrTwitter.Image = global::ClipBoardManager.Properties.Resources.twitter;
+            this.pcrTwitter.Location = new System.Drawing.Point(345, 380);
+            this.pcrTwitter.Name = "pcrTwitter";
+            this.pcrTwitter.Size = new System.Drawing.Size(25, 25);
+            this.pcrTwitter.TabIndex = 15;
+            this.pcrTwitter.TabStop = false;
+            this.pcrTwitter.Click += new System.EventHandler(this.pcrTwitter_Click);
+            // 
+            // pcrInstagram
+            // 
+            this.pcrInstagram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcrInstagram.Image = global::ClipBoardManager.Properties.Resources.instagram;
+            this.pcrInstagram.Location = new System.Drawing.Point(314, 380);
+            this.pcrInstagram.Name = "pcrInstagram";
+            this.pcrInstagram.Size = new System.Drawing.Size(25, 25);
+            this.pcrInstagram.TabIndex = 14;
+            this.pcrInstagram.TabStop = false;
+            this.pcrInstagram.Click += new System.EventHandler(this.pcrInstagram_Click);
+            // 
+            // pcrGithub
+            // 
+            this.pcrGithub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcrGithub.Image = global::ClipBoardManager.Properties.Resources.githubicon;
+            this.pcrGithub.Location = new System.Drawing.Point(283, 380);
+            this.pcrGithub.Name = "pcrGithub";
+            this.pcrGithub.Size = new System.Drawing.Size(25, 25);
+            this.pcrGithub.TabIndex = 13;
+            this.pcrGithub.TabStop = false;
+            this.pcrGithub.Click += new System.EventHandler(this.pcrGithub_Click);
+            // 
+            // pcrDiscord
+            // 
+            this.pcrDiscord.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcrDiscord.ImageLocation = "https://discord.c99.nl/widget/theme-4/892785729153933313.png";
+            this.pcrDiscord.Location = new System.Drawing.Point(137, 240);
+            this.pcrDiscord.Name = "pcrDiscord";
+            this.pcrDiscord.Size = new System.Drawing.Size(405, 85);
+            this.pcrDiscord.TabIndex = 0;
+            this.pcrDiscord.TabStop = false;
+            this.pcrDiscord.Click += new System.EventHandler(this.pcrDiscord_Click);
+            // 
+            // Contact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(662, 427);
+            this.Controls.Add(this.pcrMail);
+            this.Controls.Add(this.pcrTwitter);
+            this.Controls.Add(this.pcrInstagram);
+            this.Controls.Add(this.pcrGithub);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.gunaSeparator2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gunaSeparator1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcrDiscord);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Contact";
+            this.Text = "Contact";
+            ((System.ComponentModel.ISupportInitialize)(this.pcrMail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrTwitter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrInstagram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrGithub)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcrDiscord)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcrDiscord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer DiscordBannerController;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI.WinForms.GunaSeparator gunaSeparator1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private Guna.UI.WinForms.GunaSeparator gunaSeparator2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pcrGithub;
+        private System.Windows.Forms.PictureBox pcrInstagram;
+        private System.Windows.Forms.PictureBox pcrTwitter;
+        private System.Windows.Forms.PictureBox pcrMail;
+    }
+}
---
+++ ClipBoardManager/Forms/Contact.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipBoardManager.Forms
+{
+    public partial class Contact : Form
+    {
+        public Contact()
+        {
+            InitializeComponent();
+        }
+
+        private void DiscordBannerController_Tick(object sender, EventArgs e)
+        {
+            pcrDiscord.ImageLocation = "https://discord.c99.nl/widget/theme-4/892785729153933313.png";
+
+        }
+
+        private void pcrDiscord_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.com/users/892785729153933313");
+
+        }
+
+        private void pcrGithub_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/omerhuseyingul");
+
+        }
+
+        private void pcrInstagram_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.instagram.com/omerhuseyingul0/");
+
+        }
+
+        private void pcrTwitter_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitter.com/omerhuseyingul");
+
+        }
+
+        private void pcrMail_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:omerhuseyingul@outlook.com");
+
+        }
+    }
+}
